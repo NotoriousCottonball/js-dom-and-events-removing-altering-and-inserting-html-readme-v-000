ce592c0ed303b9140a7592802812610af86ac4ad
George  Pianka <gpianka1@gmail.com>
2019-04-27 06:51:50 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 //you can use this to paste in js code and have it run when index.html is loaded
 let element = document.createElement('div');
 
-
+element.style.textAlign = 'center';
 
 let ul = document.createElement('ul');
  
