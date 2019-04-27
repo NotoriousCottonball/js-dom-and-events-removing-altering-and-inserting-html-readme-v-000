bd08ca2c1d64e988581b77e6866ce7dfc1401685
George  Pianka <gpianka1@gmail.com>
2019-04-27 06:52:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 let element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-
+document.body.appendChild(element);
 element.style.textAlign = 'center';
 
 let ul = document.createElement('ul');
